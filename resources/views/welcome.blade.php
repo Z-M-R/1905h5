2fa962953b4e6c30523b1bf3bdfec4b86cf15851
ZMR <2417894482@qq.com>
2020-01-14 03:20:06 +0000

    网站

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    1905H5
                 </div>
 
                 <div class="links">
--- routes/web.php
+++ routes/web.php
@@ -11,6 +11,10 @@
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+// Route::get('/', function () {
+//     return view('welcome');
+// });
+
+Route::get('/','Index\IndexController@index'); //网站首页
+
+Route::get('/user/reg','User\IndexController@reg'); //首页
